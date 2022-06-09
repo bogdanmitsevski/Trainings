8d08dc6f9d46e7961da53320c1141ad3d646e76d
Bohdan Mitsevskyi <mitsevskyi.corporate@gmail.com>
2022-06-09 20:31:01 +0300

    https://github.com/bogdanmitsevski/JavaScript.git

---
+++ ifElse.js
@@ -0,0 +1,27 @@
+"use strict";
+let first = 1;
+let second = 2;
+let message = "Hello Bogdan";
+
+if(first===second){
+    console.log(message);
+}
+else if (first!==second)
+    console.log(message+message);
+
+else 
+    console.log(message+message+message);
+
+let messageStart = "Hello";
+let messageEnd = (5<1) ? ", Bogdan" : ", Stas";
+(5>5) ? ", A":", B";
+(5<6) ? ",A1":",A2";
+messageStart+=messageEnd;
+console.log(messageStart);
+
+if(0){
+    console.log("True");
+}
+else if(" "){
+    console.log("False");
+}
